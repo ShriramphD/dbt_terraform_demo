33aebe55f3a1b53c48864e6ec2e2e2e2c6a29e2e
ShriramphD <sshriram@phdata.io>
2025-03-11 10:42:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,33 +1,36 @@
 variable "dbt_account_id" {
   description = "Your dbt Cloud account ID"
   type        = number
+  default     = 123456  # Replace with your actual dbt Cloud account ID
 }
 
 variable "dbt_api_key" {
   description = "Your dbt Cloud API key"
   type        = string
   sensitive   = true
+  default     = "your-dbt-api-key-here"  # Replace with your actual API key
 }
 
 variable "dbt_project_name" {
   description = "The name of the dbt Cloud project"
   type        = string
-  default     = "Terraform_Demo_Project"
+  default     = "Insurance Data Warehouse"
 }
 
 variable "dbt_environment_name" {
   description = "Environment name for dbt Cloud"
   type        = string
-  default     = "Development"
+  default     = "Production"
 }
 
 variable "dbt_repository_url" {
   description = "GitHub repo URL for dbt project"
   type        = string
+  default     = "https://github.com/your-org/dbt-insurance-project.git"  # Replace with actual GitHub repo
 }
 
 variable "dbt_job_name" {
   description = "Job name for dbt run"
   type        = string
-  default     = "dbt Production Run"
+  default     = "Insurance Data Refresh Job"
 }
